2a95264d178e08376835e8a9024d68595a24d345
Optimus <3300532@gmail.com>
2023-07-06 16:15:00 +0600

    slog logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,22 +1,52 @@
 package main
 
 import (
-	"fmt"
+	"os"
 
 	"github.com/aidos-dev/url-shortener/internal/config"
+	"golang.org/x/exp/slog"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
-	// TODO: init config: cleanenv
+	// init config: cleanenv
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	// init logger: slog
+	log := setupLogger(cfg.Env)
 
-	// TODO: init logger: slog
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
-	// TODO: init storage: sqlite3
+	// init storage: sqlite3
 
-	// TODO: init router: chi, "chi render"
+	// init router: chi, "chi render"
 
-	// TODO: run server
+	// run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
