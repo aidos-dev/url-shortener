1623bf5c4dd532b9e18a919707a7140af73828b0
Optimus <3300532@gmail.com>
2023-07-06 14:52:33 +0600

    config

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/aidos-dev/url-shortener/internal/config"
+
+func main() {
+	// TODO: init config: cleanenv
+	cfg := config.MustLoad()
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: sqlite3
+
+	// TODO: init router: chi, "chi render"
+
+	// TODO: run server
+}
