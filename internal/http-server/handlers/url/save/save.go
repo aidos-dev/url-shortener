045cb3239e52ed97878f0ff90f025cc530356a21
Optimus <3300532@gmail.com>
2023-07-08 17:49:19 +0600

    calling SaveURL func from hendler

--- internal/http-server/handlers/url/save/save.go
+++ internal/http-server/handlers/url/save/save.go
@@ -1,11 +1,13 @@
 package save
 
 import (
+	"errors"
 	"net/http"
 
 	resp "github.com/aidos-dev/url-shortener/internal/lib/api/response"
 	"github.com/aidos-dev/url-shortener/internal/lib/logger/sl"
 	"github.com/aidos-dev/url-shortener/internal/lib/random"
+	"github.com/aidos-dev/url-shortener/internal/storage"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
 	"github.com/go-playground/validator/v10"
@@ -65,5 +67,32 @@ func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
 		if alias == "" {
 			alias = random.NewRandomString(aliasLength)
 		}
+
+		id, err := urlSaver.SaveURL(req.URL, alias)
+		if errors.Is(err, storage.ErrURLExists) {
+			log.Info("url already exists", slog.String("url", req.URL))
+
+			render.JSON(w, r, resp.Error("url already exists"))
+
+			return
+		}
+		if err != nil {
+			log.Error("failed to add url", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("failed to add url"))
+
+			return
+		}
+
+		log.Info("url added", slog.Int64("id", id))
+
+		responseOK(w, r, alias)
 	}
+}
+
+func responseOK(w http.ResponseWriter, r *http.Request, alias string) {
+	render.JSON(w, r, Response{
+		Response: resp.OK(),
+		Alias:    alias,
+	})
 }
